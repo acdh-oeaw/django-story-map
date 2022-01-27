16e8104385e28794e685cdfebc2a67dcba52cc04
csae8092 <peter.andorfer@oeaw.ac.at>
2022-01-27 15:43:48 +0100

    added json-data endpoint

--- story_map/urls.py
+++ story_map/urls.py
@@ -5,4 +5,5 @@ app_name = 'story_map'
 
 urlpatterns = [
     path('<int:pk>/', views.StoryDetailView.as_view()),
+    path('data/<int:pk>/', views.StoryJsonData.as_view()),
 ]
--- story_map/views.py
+++ story_map/views.py
@@ -1,5 +1,5 @@
+from django.http import JsonResponse
 from django.views.generic.detail import DetailView
-
 
 from . models import Story
 
@@ -7,3 +7,11 @@ from . models import Story
 class StoryDetailView(DetailView):
     model = Story
     template_name = 'story_map/story_detail.html'
+
+
+class StoryJsonData(DetailView):
+    model = Story
+
+    def render_to_response(self, context, **kwargs):
+        data = self.object.payload
+        return JsonResponse(data)
